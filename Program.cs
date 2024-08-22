804df6c2d41683ef8f06765ab32dd7453835115c
Justin Didelot <163373490+Srekaens@users.noreply.github.com>
2024-08-22 11:50:45 +0200

    Ajout du program.cs

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+static string CreateChessboard(int size)
+{
+    StringBuilder board = new StringBuilder();
+
+    for (int row = 0; row < size; row++)
+    {
+        for (int col = 0; col < size; col++)
+        {
+            if ((row + col) % 2 == 0)
+            {
+                board.Append(' ');
+            }
+            else
+            {
+                board.Append('#');
+            }
+        }
+        board.AppendLine();
+    }
+
+    return board.ToString();
+}
+
+    int size = 4;
+    Console.WriteLine(CreateChessboard(size));
+
